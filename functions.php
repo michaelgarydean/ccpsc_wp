a06f1d71c61f0a3858c7fcb01abed56efee96bd8
Mike Dean <mike@wpdev0.koumbit.net>
2020-11-11 20:53:44 -0500

    added featured boxes updates on the homepage

--- functions.php
+++ functions.php
@@ -13,10 +13,10 @@ add_action( 'wp_enqueue_scripts', 'anima_parent_theme_enqueue_styles' );
  * Enqueue scripts and styles.
  */
 function anima_parent_theme_enqueue_styles() {
-	wp_enqueue_style( 'anima-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'anima-plus-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'ccpsc-style',
 		get_stylesheet_directory_uri() . '/style.css',
-		array( 'anima-style' )
+		array( 'anima-plus-style' )
 	);
 
 }
